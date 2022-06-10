bb6d032fab5c7ca3452ba9d0fb6d96a70bb001df
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-06-10 10:40:59 +0200

    added answers to all requested tasks in card.js, header.js and tabs.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,33 +1,76 @@
-const Card = (article) => {
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
-}
+import axios from "axios";
+// TASK 5
+// ---------------------
+// Implement this function, which should return the markup you see below.
+// It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
+// The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+// Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
+//
+// <div class="card">
+//   <div class="headline">{ headline }</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={ authorPhoto }>
+//     </div>
+//     <span>By { authorName }</span>
+//   </div>
+// </div>
+//
+const Card = (articles) => {
+  // creating DOM elements
+  // articles.forEach(element => {
 
+  // });
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+  const headlineDiv = document.createElement("div");
+  headlineDiv.classList.add("headline");
+  headlineDiv.textContent = articles.headline;
+
+  const authorDiv = document.createElement("div");
+  authorDiv.classList.add("author");
+
+  const imageDiv = document.createElement("div");
+  imageDiv.classList.add("img-container");
+
+  const image = document.createElement("img");
+  image.src = articles.authorPhoto;
+
+  const span = document.createElement("span");
+  span.textContent = `By ${articles.authorName}`;
+
+  //appending elements to partents
+  cardDiv.appendChild(headlineDiv);
+  cardDiv.appendChild(authorDiv);
+  authorDiv.appendChild(imageDiv);
+  imageDiv.appendChild(image);
+  authorDiv.appendChild(span);
+
+  cardDiv.addEventListener("click", (e) => {
+    console.dir(e.target.textContent);
+  });
+
+  return cardDiv;
+};
+
+// TASK 6
+// ---------------------
+// Implement this function that takes a css selector as its only argument.
+// It should obtain articles from this endpoint: `http://localhost:5001/api/articles` (test it with console.log!!).
+// However, the articles do not come organized in a single, neat array. Inspect the response closely!
+// Create a card from each and every article object in the response, using the Card component.
+// Append each card to the element in the DOM that matches the selector passed to the function.
+//
 const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5001/api/articles` (test it with console.log!!).
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-}
-
-export { Card, cardAppender }
+  axios.get("http://localhost:5001/api/articles").then((res) => {
+    console.log(res);
+    const bigArray = [];
+    bigArray.push(Object.values(res.data.articles));
+    const flattenedArray = bigArray.flat(2);
+    flattenedArray.forEach((element) => {
+      document.querySelector(selector).appendChild(Card(element));
+    });
+  });
+};
+export { Card, cardAppender };
--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,50 @@
+// TASK 1
+// ---------------------
+// Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
+// The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+//
+//  <div class="header">
+//    <span class="date">{ date }</span>
+//    <h1>{ title }</h1>
+//    <span class="temp">{ temp }</span>
+//  </div>
+//
 const Header = (title, date, temp) => {
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
-}
+  //creating DOM elements,assigning classes and text content
+  const headerDiv = document.createElement("div");
+  headerDiv.classList.add("header");
+
+  const dateSpan = document.createElement("span");
+  dateSpan.classList.add("date");
+  dateSpan.textContent = date;
+
+  const headerTitle = document.createElement("h1");
+  headerTitle.textContent = title;
+
+  const tempSpan = document.createElement("span");
+  tempSpan.classList.add("temp");
+  tempSpan.innerHTML = temp;
+
+  //Appending elements to parents
+  headerDiv.appendChild(dateSpan);
+  headerDiv.appendChild(headerTitle);
+  headerDiv.appendChild(tempSpan);
+
+  return headerDiv;
+};
+
+// TASK 2
+// ---------------------
+// Implement this function taking a css selector as its only argument.
+// It should create a header using the Header component above, passing arguments of your choosing.
+// It should append the header to the element in the DOM that matches the given selector.
+//
 
 const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
-}
+  document
+    .querySelector(selector)
+    .appendChild(Header("BloomTech Times", "June 10, 2022", "86&deg F"));
+};
 
-export { Header, headerAppender }
+export { Header, headerAppender };
